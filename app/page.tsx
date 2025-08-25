635e6891eb7b803864b404a134c5c11766579fa9
Daniel Chen <d.chen@live.com>
2025-08-25 15:42:13 +0800

    feat: change references to Daniel

--- app/page.tsx
+++ app/page.tsx
@@ -2,20 +2,22 @@ import { BlogPosts } from 'app/components/posts'
 
 export default function Page() {
   return (
-    <section>
-      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        My Portfolio
-      </h1>
-      <p className="mb-4">
-        {`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
-        Vim's keystroke commands and tabs' flexibility for personal viewing
-        preferences. This extends to my support for static typing, where its
-        early error detection ensures cleaner code, and my preference for dark
-        mode, which eases long coding sessions by reducing eye strain.`}
-      </p>
-      <div className="my-8">
-        <BlogPosts />
-      </div>
-    </section>
+    <>
+      <section>
+        <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
+          Daniel Chen
+        </h1>
+        <p className="mb-4">
+          Hi 👋 My name is Daniel, I am a Professional Full-Stack Engineer with also a bit of experience with Machine Learning.
+          <br /><br />
+        </p>
+        <h2>My Favourite Technologies:</h2>
+        Frontend - Angular, Next JS
+        <br />
+        Backend - Spring Boot (Java), Django (Python)
+        <br />
+        Code Editors - Cursor, VIM (Although I mainly just use the shortcuts in other editors nowadays)
+      </section>
+    </>
   )
 }
